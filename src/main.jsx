681b1c168910180e28230269135b3c1e5898b89b
JathinSekhar <nerellajathin2006@gmail.com>
2025-09-22 10:23:08 +0530

    your message

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Forgot from "./pages/Forget";          // fixed
+import Forgot from "./pages/Forget";
 import Dashboard from "./Dashboard.jsx";
 import ResetPassword from "./pages/Resetpassword.jsx";
 
@@ -13,10 +13,10 @@ import Expense from "./DashPage/Expense";
 import Transaction from "./DashPage/Transaction";
 import SetBudget from "./DashPage/SetBudget";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    {/* 👇 Add basename so all routes work under /myexpense */}
+    <Router basename="/myexpense">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
